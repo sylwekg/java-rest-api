ddab1def8d2cd06dc3c3736abb63f43c5a8ad19b
Sylwester Guzek <sylwester.guzek@wp.pl>
2018-07-05 16:36:59 +0200

    add fields created_at, upated_at to task

--- src/main/java/com/auth0/samples/authapi/task/Task.java
+++ src/main/java/com/auth0/samples/authapi/task/Task.java
@@ -1,11 +1,19 @@
 package com.auth0.samples.authapi.task;
 
 import com.auth0.samples.authapi.user.ApplicationUser;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+		allowGetters = true)
 public class Task {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,8 +22,15 @@ public class Task {
 	@NotNull
 	private String description;
 
-//	@CreationTimestamp
-//	private LocalDateTime createDateTime;
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date createdAt;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date updatedAt;
 
 	@ManyToOne
 	private ApplicationUser owner;
@@ -44,5 +59,21 @@ public class Task {
 
 	public void setOwner(ApplicationUser owner) {
 		this.owner = owner;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
 	}
 }
